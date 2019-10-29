85b0b0b04933e805ade4332ebbbe574e3bb7abd3
Youssef D <youssefdbouk@icloud.com>
2019-10-29 09:40:15 -0500

    Require Old Password to Change

--- htdocs/acc/scripts/changepwd.script.php
+++ htdocs/acc/scripts/changepwd.script.php
@@ -4,6 +4,7 @@ if (isset($_POST['pwd-submit'])) {
   session_start();
   require '../../scripts/handledb.script.php';
 
+  $oldPassword = $_POST['oldpsw']
   $password = $_POST['psw'];
   $confirmPassword = $_POST['psw2'];
   $userID = $_SESSION['userid'];
@@ -25,7 +26,14 @@ if (isset($_POST['pwd-submit'])) {
     } else {
       mysqli_stmt_bind_param($stmt, "s", $userID);
       mysqli_stmt_execute($stmt);
-      mysqli_stmt_store_result($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+			if ($row = mysqli_fetch_assoc($result)) {
+				$pwdCheck = password_verify($oldPassword, $row['pwdUsers']);
+				if ($pwdCheck == false) {
+					header("Location: ../changepwd/index.php?result=pwdnogo");
+					exit();
+				}
+      }
       $sql = "UPDATE login_data SET pwdUsers=? WHERE idUsers=?";
       $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -35,8 +43,9 @@ if (isset($_POST['pwd-submit'])) {
           $hashpwd = password_hash($password, PASSWORD_DEFAULT);
           mysqli_stmt_bind_param($stmt, "ss", $hashpwd, $userID );
           mysqli_stmt_execute($stmt);
-
-          header("Location: ../../console");
+          session_unset();
+          session_destroy();
+          header("Location: ../login/index.php?result=changedpwd");
           exit();
         }
       }
