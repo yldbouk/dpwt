96aaf5873a7989f8d28e62c6dd4467424678f745
Youssef D <youssefdbouk@icloud.com>
2019-12-06 11:38:21 -0600

    Update Extras Repo Name

--- htdocs/console/dev/push.php
+++ htdocs/console/dev/push.php
@@ -1,4 +1,4 @@
 <?php
 
 shell_exec('cd C:\xampp && git reset --hard HEAD && git pull');
-shell_exec('cd C:\xampp\htdocs\Tu0xYNqK1hdFxmAMsjeX46rV482ANKij && git reset --hard HEAD && git pull');
+shell_exec('cd C:\xampp\htdocs\extras && git reset --hard HEAD && git pull');
