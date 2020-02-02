f84692073e1dc3ef452cf171d447ab51fcd99f43
Youssef D <youssefdbouk@icloud.com>
2020-02-02 12:51:51 -0600

    Revert "Debug for HTTP 301"
    This reverts commit b95a6a5d37849ec5341ff5856ee46469038f403e.

--- htdocs/acc/scripts/login.php
+++ htdocs/acc/scripts/login.php
@@ -7,14 +7,12 @@ if (isset($_POST['login-submit'])) {
 	$password = $_POST['psw'];
 
 	if (empty($username) || empty($password)) {
-		kill("test1");
 		header("Location: ../login?result=incomplete&user=".$username);
 		exit();
 	} else {
 		$sql = "SELECT * FROM login_data WHERE uidUsers=?;";
 		$stmt = mysqli_stmt_init($conn);
 		if (!mysqli_stmt_prepare($stmt, $sql)) {
-			kill("test2");
 			header("Location: ../login?result=sqlerror&user=".$username);
 			exit();
 		} else {
@@ -23,27 +21,22 @@ if (isset($_POST['login-submit'])) {
 			$result = mysqli_stmt_get_result($stmt);
 			if ($row = mysqli_fetch_assoc($result)) {
 				if ($row["permsUsers"] == "deleted") {
-					kill("test3");
 					header("Location: ../login?result=accdel");
 					exit();
 				}
 				$pwdCheck = password_verify($password, $row['pwdUsers']);
 				if ($pwdCheck == false) {
-					kill("test4");
 					header("Location: ../login?result=pwdnogo");
 					exit();
 				}
 				elseif($pwdCheck == true) {
 					if ($row["permsUsers"] == "awaitingAction") {
-						kill("test5");
 						header("Location: ../login?result=awaitingAction");
 					}
 					elseif($row["permsUsers"] == "none") {
-						kill("test6");
 						header("Location: ../login?result=noperms");
 					}
 					elseif($row["permsUsers"] == "revoked") {
-						kill("test7");
 						header("Location: ../login?result=revoke");
 					} else {
 						session_start();
@@ -54,18 +47,15 @@ if (isset($_POST['login-submit'])) {
 						$_SESSION['userMail'] = $row["emailUsers"];
 						$_SESSION['lastUpdated'] = $row["lastUpdated"];
 						if ($row["permsUsers"] == "newUser") {
-							kill("test8");
 							header("Location: ../newuser");
 							exit();
 						} else header("Location: ../../console");
 					}
 				} else {
-					kill("test9");
 					header("Location: ../login?result=pwdnogo");
 					exit();
 				}
 			} else {
-				kill("test1a");
 				header("Location: ../login?result=usrname");
 				exit();
 			}
@@ -73,6 +63,5 @@ if (isset($_POST['login-submit'])) {
 	}
 
 } else {
-	kill("test1b");
 	header("Location: ../login");
 }
