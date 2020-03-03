07a8f7c99bed2d8c47b04cdd1f28cfbec2c06a7f
Youssef D <youssefdbouk@icloud.com>
2020-03-02 18:42:47 -0600

    Update index.php

--- htdocs/console/view3d/index.php
+++ htdocs/console/view3d/index.php
@@ -15,7 +15,8 @@
   <link rel="stylesheet" href="/css/header.css">
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
   <script src="stl_viewer.min.js"></script>
-  <script>
+  <script>var stl_viewer=
+  new StlViewer(document.getElementById("stl_cont"), { models: [ {id:1, filename:"./openFile.script.php?fileID=<?php echo $id; ?>&extension=<?php echo $extension; ?>"} ] });
     window.onload = function() {
       <?php
       //add more colors in the future
@@ -24,11 +25,8 @@
       else if ($_SESSION["filamentColor"] == "red")
         $color = "ff0000";
       ?>
-      var stl_viewer=new StlViewer(document.getElementById("stl_cont"), { models: [ {id:1, filename:"./openFile.script.php?fileID=<?php echo $id; ?>&extension=<?php echo $extension; ?>"} ] });
-      window.onload=function(){
-        stl_viewer.set_color(1, '<?php echo $color; ?>');
-      }
       
+      stl_viewer.set_color(1, '<?php echo $color; ?>');
     }
   </script>
 </head>
