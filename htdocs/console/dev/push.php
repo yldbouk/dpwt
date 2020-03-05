b9b1bc05de13aeaf91537be188c41dff264bbfde
Youssef D <youssefdbouk@icloud.com>
2020-03-05 07:56:52 -0600

    Update push.php

--- htdocs/console/dev/push.php
+++ htdocs/console/dev/push.php
@@ -1,6 +1,6 @@
 <?php
 
-exec('cd /opt/lampp/lampp && git reset --hard && git pull 2>&1', $output);
+shell_exec('cd /opt/lampp/lampp && git reset --hard && git pull 2>&1', $output);
             print_r($output);  
-exec('cd /opt/lampp/lampp/htdocs/extras && git reset --hard && git pull 2>&1', $output);
+shell_exec('cd /opt/lampp/lampp/htdocs/extras && git reset --hard && git pull 2>&1', $output);
             print_r($output);  
