7dfe6ea576aaf082d07eb4af3f21cc0b5892002c
Youssef D <youssefdbouk@icloud.com>
2020-03-09 18:01:50 -0500

    idk

--- htdocs/console/requestprint/scripts/createRequest.script.php
+++ htdocs/console/requestprint/scripts/createRequest.script.php
@@ -44,7 +44,7 @@ require "../../scripts/handledb.script.php";
   } else {
     header("Location: ../index.php?result=printer");
     exit();
-}
+  }
 
   if ($name == "T.DONOTMODIFY" || $reason == "T.DONOTMODIFY"){
     header("Location: ../index.php?result=servername");
@@ -53,7 +53,7 @@ require "../../scripts/handledb.script.php";
     $sql = "SELECT jobName FROM job_data WHERE jobName=?";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-\r("Location: ../index.php?result=sqlerror");
+      ("Location: ../index.php?result=sqlerror");
       exit();
     } else {
       mysqli_stmt_bind_param($stmt, "s", $name);
@@ -83,70 +83,56 @@ require "../../scripts/handledb.script.php";
                 mysqli_stmt_execute($stmt);
               }
             }
-            $sql = "SELECT * FROM job_data WHERE jobName=?";
-            $stmt = mysqli_stmt_init($conn);
-            if (!mysqli_stmt_prepare($stmt, $sql)) {
-              header("Location: ../index.php?result=sqlerror");
+            $id = $row['id'];
+            $target_dir = "../../uploads/".$id."/";
+            $target_file = $target_dir . basename($_FILES["stlobj"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            if (file_exists($target_file)) {
+              echo "Sorry, file already exists.";
+              $uploadOk = 0;
+          }
+            // Check file size
+            if ($_FILES["stlobj"]["size"] > 5000000) {
+              header("Location: ../requestprint/index.php?result=filesize");
+              $uploadOk = 0;
               exit();
+          }
+          // Allow certain file formats
+          if($imageFileType != "stl" && $imageFileType != "obj" ) {
+            header("Location: ../requestprint/index.php?result=filetype");
+              $uploadOk = 0;
+              exit();
+          }
+            $fileLocation = "../../uploads/".$id;
+            mkdir($fileLocation, 0700);
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
             } else {
-              mysqli_stmt_bind_param($stmt, "s", $name);
-              mysqli_stmt_execute($stmt);
-              $result = mysqli_stmt_get_result($stmt);
-              if ($row = mysqli_fetch_assoc($result)) {
-                $id = $row['id'];
-                $target_dir = "../../uploads/".$id."/";
-                $target_file = $target_dir . basename($_FILES["stlobj"]["name"]);
-                $uploadOk = 1;
-                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-                if (file_exists($target_file)) {
-                  echo "Sorry, file already exists.";
-                  $uploadOk = 0;
-              }
-                // Check file size
-                if ($_FILES["stlobj"]["size"] > 5000000) {
-                  header("Location: ../requestprint/index.php?result=filesize");
-                  $uploadOk = 0;
-                  exit();
-              }
-              // Allow certain file formats
-              if($imageFileType != "stl" && $imageFileType != "obj" ) {
-                header("Location: ../requestprint/index.php?result=filetype");
-                  $uploadOk = 0;
-                  exit();
-              }
-                $fileLocation = "../../uploads/".$id;
-                mkdir($fileLocation, 0700);
+                if (move_uploaded_file($_FILES["stlobj"]["tmp_name"], $target_file)) {
+                  rename("../../uploads/".$id."/".$_FILES['stlobj']['name'], "../../uploads/".$id."/".$id.".".$imageFileType);
+                  $sql = "UPDATE `job_data` SET `fileLocation`=? WHERE `job_data`.`id`=?";
+                  if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../requestprint/index.php?result=sqlerror");
+                    exit();
+                  } else {
+                    mysqli_stmt_bind_param($stmt, "ss", $fileLocation, $id);
+                    mysqli_stmt_execute($stmt);
+                    if ($status == 'queue') copy('../../uploads/'.$id."/".$id.".stl", '../../uploads/__queue/'.$id.".stl");
 
-                // Check if $uploadOk is set to 0 by an error
-                if ($uploadOk == 0) {
-                    echo "Sorry, your file was not uploaded.";
-                // if everything is ok, try to upload file
-                } else {
-                    if (move_uploaded_file($_FILES["stlobj"]["tmp_name"], $target_file)) {
-                      rename("../../uploads/".$id."/".$_FILES['stlobj']['name'], "../../uploads/".$id."/".$id.".".$imageFileType);
-                      $sql = "UPDATE `job_data` SET `fileLocation`=? WHERE `job_data`.`id`=?";
-                      if (!mysqli_stmt_prepare($stmt, $sql)) {
-                        header("Location: ../requestprint/index.php?result=sqlerror");
-                        exit();
-                      } else {
-                        mysqli_stmt_bind_param($stmt, "ss", $fileLocation, $id);
-                        mysqli_stmt_execute($stmt);
-                        if ($status == 'queue') copy('../../uploads/'.$id."/".$id.".stl", '../../uploads/__queue/'.$id.".stl");
-
-                        
-                        header("Location: ../../viewJobs/index.php?edit=".$id);
+                    
+                    header("Location: ../../viewJobs/index.php?edit=".$id);
 
 
-                        
-                      }
-                    } else {
-                        echo "Sorry, there was an error uploading your file.";
-                    }
+                 
                   }
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
                 }
-            }
+              }
           }
         }
       }
     }
-  
