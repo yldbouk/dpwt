29ba590214299dab403424a9dd0ff8e8072c1fdd
Youssef D <youssefdbouk@icloud.com>
2020-03-10 12:02:25 -0500

    Do the thing

--- htdocs/console/viewJobs/scripts/editJobs.script.php
+++ htdocs/console/viewJobs/scripts/editJobs.script.php
@@ -235,23 +235,36 @@ if(isset($_POST['edit-pause'])) {
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
         if ($row = mysqli_fetch_assoc($result)) { 
-          $queue = $row['jobQueue'];
+          $on = $row['jobQueue'];
           echo($queue);
           $sql = "SELECT MAX(jobQueue) AS jobQueue FROM job_data WHERE jobQueue<?;";
           if (!mysqli_stmt_prepare($stmt, $sql)) {
             header("Location: ../../error.php/?e=internal");
             exit();
           } else {
-            mysqli_stmt_bind_param($stmt, "s", $queue);
+            mysqli_stmt_bind_param($stmt, "s", $on);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
         if ($row = mysqli_fetch_assoc($result))
-         die('"'.$row['jobQueue'].'"');
-   
+         $before = $row['jobQueue'];
+         //calculate mean
+            $added = $on + $before;
+            $mean = $added / 2;
+            
+            $sql = "UPDATE `job_data` SET jobQueue =? WHERE id=?;";
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+              header("Location: ../../error.php/?e=internal");
+              exit();
+            } else {
+              mysqli_stmt_bind_param($stmt, "ss", $mean, $id);
+              mysqli_stmt_execute($stmt);
+            
             usleep(250);
             echo "<script>history.go(-1);</script>";
-          }
-        } 
+          
+            }
+          } 
+        }
       } 
     } else {
   die("No Action to Take.");
