4cedad35bea152383104fcddcfe9db3d609fd6aa
Youssef D <youssefdbouk@icloud.com>
2020-03-10 08:34:23 -0500

    Update editJobs.script.php

--- htdocs/console/viewJobs/scripts/editJobs.script.php
+++ htdocs/console/viewJobs/scripts/editJobs.script.php
@@ -225,6 +225,34 @@ if(isset($_POST['edit-pause'])) {
           }
         } 
       } 
+    } else if (isset($_POST['edit-queue'])) {
+      $sql = "SELECT * FROM job_data WHERE id=?;";
+      if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: ../../error.php/?e=internal");
+        exit();
+      } else {
+        mysqli_stmt_bind_param($stmt, "s", $id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if ($row = mysqli_fetch_assoc($result)) { 
+          $queue = $row['jobQueue'];
+          
+          $sql = "SELECT MAX(jobQueue) FROM job_data WHERE id=? AND jobQueue<?;";
+          if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../../error.php/?e=internal");
+            exit();
+          } else {
+            mysqli_stmt_bind_param($stmt, "ss", $do, $queue);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+        if ($row = mysqli_fetch_assoc($result)) { 
+         echo($row['jobQueue']) 
+        }
+            usleep(250);
+            echo "<script>history.go(-1);</script>";
+          }
+        } 
+      } 
     } else {
   die("No Action to Take.");
 }
