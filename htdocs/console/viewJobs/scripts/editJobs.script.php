634811609e03cc3000d4a6c2e356f29ec0d184a8
Youssef D <youssefdbouk@icloud.com>
2020-03-10 08:42:50 -0500

    Testing

--- htdocs/console/viewJobs/scripts/editJobs.script.php
+++ htdocs/console/viewJobs/scripts/editJobs.script.php
@@ -226,7 +226,7 @@ if(isset($_POST['edit-pause'])) {
         } 
       } 
     } else if (isset($_POST['edit-queue'])) {
-      $sql = "SELECT * FROM job_data WHERE id=?;";
+      $sql = "SELECT jobQueue FROM job_data WHERE id=?;";
       if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("Location: ../../error.php/?e=internal");
         exit();
@@ -246,7 +246,7 @@ if(isset($_POST['edit-pause'])) {
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
         if ($row = mysqli_fetch_assoc($result)) { 
-         echo($row['jobQueue']) 
+         die($row['jobQueue']) 
         }
             usleep(250);
             echo "<script>history.go(-1);</script>";
